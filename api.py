a924f837fdb52b4aa007dff0136c70d97ded1e81
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-03 00:15:07 +0900

    :+1: Add CORS Handling and remove unnecessary code

--- api.py
+++ api.py
@@ -1,36 +1,22 @@
-import json
 import configparser
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.params import Depends
-from fastapi.routing import APIRouter
+from fastapi import FastAPI
 from pydantic import BaseModel
-import time
-from typing import Any, Dict, List, Optional, Union, final
 import mysql.connector as mydb
 
-from starlette.responses import JSONResponse 
+from starlette.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-# app_router = APIRouter()
 
-class BrowserHistory(BaseModel):
-    id: Optional[str] = ''
-    lastVisitTime: Optional[float] = 0.
-    title: str
-    typedCount: Optional[int] = 0
-    url: str
-    visitCount: Optional[int] = 0
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-class ExtensionBaseRequest(BaseModel): 
-    ext_id: str
-
-class HistoryRequestBody(ExtensionBaseRequest):
-    data: List[BrowserHistory]
-    description: Optional[str] = ''
-    def jsonize(self): return {"data": self.data, "description": self.description, "ext_id": self.ext_id}
-
-class UserLoggingRequest(ExtensionBaseRequest):
+class UserLoggingRequest(BaseModel):
     id: str
     uid: str
     timeOnPage: int
@@ -45,8 +31,8 @@ def get_db_connection():
         host=_parser["mysql"]["host"],
         port=_parser["mysql"]["port"],
         user=_parser["mysql"]["user"],
-        password=_parser["mysql"]["passwd"],
-        database=_parser["mysql"]["db"]
+        password=_parser["mysql"]["password"],
+        database=_parser["mysql"]["database"]
     )
 
 @app.post('/storelog')
@@ -56,6 +42,7 @@ async def post_user_log(request: UserLoggingRequest):
     """
     connection = get_db_connection()
     cursor = connection.cursor()
+    status = False
 
     try:
         cursor.execute('INSERT INTO logs VALUES(%s, %s, %s, %s, %s)', (request.id, request.uid, request.timeOnPage, request.pageUrl, request.linkedPageNum))
@@ -63,45 +50,14 @@ async def post_user_log(request: UserLoggingRequest):
         print(e)
         status = False
     else: 
-        cursor.commit()
         status = True
+        cursor.commit()
     finally:
         cursor.close()
         connection.close()
 
-    return {'status': status}
+    return JSONResponse(content={'status': status})
 
-@app.middleware('http')
-async def add_cors_header(request: Request, call_next):
-    """
-    In middleware, it seems to block application to execute `await request.json()` or something read request.
-    """
-    start_time = time.time()
-    response = await call_next(request)
-    # if ext_id: response.headers['Access-Control-Allow-Origin'] = f'"chrome-extension://{ext_id}/"'
-    # response.headers['Access-Control-Allow-Methods'] = '"post"'
-    # response.headers['Access-Control-Allow-Headers'] = '"X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept"'
-    process_time = time.time() - start_time
-    response.headers['X-Process-Time'] = str(process_time)
-    return response
-
-def add_cors_header(response: JSONResponse, ext_id: str):
-    response.headers['Access-Control-Allow-Origin'] = f'chrome-extension://{ext_id}'
-    response.headers['Vary'] = 'Origin'
-    response.headers['Access-Control-Allow-Methods'] = 'post'
-    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept'
-    return response
-
-# @app_router.post('/ping')
 @app.post('/ping')
-async def ping_pong(request: ExtensionBaseRequest):
-    ext_id = request.ext_id
-    request_json = request.json()
-    response = JSONResponse(request_json)
-    response.headers['Access-Control-Allow-Origin'] = f'chrome-extension://{ext_id}'
-    response.headers['Vary'] = 'Origin'
-    response.headers['Access-Control-Allow-Methods'] = 'post'
-    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept'
-    return response 
-
-# app.include_router(app_router, dependencies=[Depends(add_cors_header)])
+async def ping_pong(request: BaseModel):
+    return request 
